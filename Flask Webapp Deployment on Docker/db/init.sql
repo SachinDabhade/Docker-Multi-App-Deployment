c39c841d27558eb3ece88405d2bf718441db7368
Sachin Vinayak Dabhade <78628027+SachinDabhade@users.noreply.github.com>
2025-02-24 12:33:40 +0530

    Update init.sql

--- Flask Webapp Deployment on Docker/db/init.sql
+++ Flask Webapp Deployment on Docker/db/init.sql
@@ -1,12 +1,1 @@
-CREATE DATABASE IF NOT EXISTS user_db;
 
-USE user_db;
-
-CREATE TABLE IF NOT EXISTS users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(50) NOT NULL,
-    password VARCHAR(50) NOT NULL
-);
-
--- Add a sample user (username: admin, password: password123)
-INSERT INTO users (username, password) VALUES ('admin', 'password123');
